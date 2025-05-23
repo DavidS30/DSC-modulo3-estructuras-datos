d87defe62dfeaea85a3e60b65c8a4dd793abc699
David Salas <davidsalas3099@gmail.com>
2025-05-22 21:20:10 -0500

    feat: first class content.

---
+++ clase1_listas_tuplas/ejemplos/slicing_comprehension.py
@@ -0,0 +1,15 @@
+# Slicing
+letras = ["a", "b", "c", "d", "e"]
+print(letras[1:4])  # ["b", "c", "d"]
+print(letras[:3])   # ?
+print(letras[-2:])  # ?
+
+cuadrados = [x**2 for x in range(5)]
+print(cuadrados)  # [0, 1, 4, 9, 16]
+
+# equivalente a
+cuadrados = []
+for x in range(5):
+    cuadrados.append(x**2)
+print(cuadrados)  # [0, 1, 4, 9, 16]
+
