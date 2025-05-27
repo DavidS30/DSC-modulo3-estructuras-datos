a40fa19ee6c5fd4057bc9f661f1cf8c3d9672425
David Salas <davidsalas3099@gmail.com>
2025-05-26 23:30:50 -0500

    feat: clase tutoria 1.

--- clase1_listas_tuplas/ejemplos/slicing_comprehension.py
+++ clase1_listas_tuplas/ejemplos/slicing_comprehension.py
@@ -4,12 +4,12 @@ print(letras[1:4])  # ["b", "c", "d"]
 print(letras[:3])   # ?
 print(letras[-2:])  # ?
 
-cuadrados = [x**2 for x in range(5)]
+cuadrados = [x**2 for x in range(6)]
 print(cuadrados)  # [0, 1, 4, 9, 16]
 
 # equivalente a
 cuadrados = []
 for x in range(5):
-    cuadrados.append(x**2)
+    cuadrados.append(x**2) 
 print(cuadrados)  # [0, 1, 4, 9, 16]
 
---
+++ clase1_tutoria/Ejercicio1.py
@@ -0,0 +1,4 @@
+#''
+#Desafío de listas dinámicas:
+# ingresar N productos y generar un reporte limpio.
+#''
